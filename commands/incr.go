36095f4e349ac377a54e6a7ca37dd6ac4ec06e53
Alperen Turkoz <alperentrkz@gmail.com>
2020-03-01 00:54:08 +0300

    implement incr cmd

--- commands/incr.go
+++ commands/incr.go
@@ -1,7 +1,52 @@
 package commands
 
-import "go-kvdb/database"
+import (
+	"go-kvdb/database"
+	"strconv"
+	"strings"
+)
 
+var (
+	errIncrKeyNotDefRes    = Response{StatusErr, "Key field is not defined in received arguments", nil}
+	errIncrKeyNotExistsRes = Response{StatusErr, "There is not any data related with the received key.", nil}
+	errIncrParseErrRes     = Response{StatusErr, "Unable to parse the data into number type", nil}
+)
 var Incr CommandFunc = func(db *database.Database, m map[string]string) (Response, bool) {
-	return Response{}, true
+	key, keyExists := m["key"]
+
+	if !keyExists {
+		return errIncrKeyNotDefRes, false
+	}
+
+	val, valExists := db.Get(key)
+
+	if !valExists {
+		return errIncrKeyNotExistsRes, false
+	}
+
+	var finalVal string
+
+	if strings.Contains(val, ".") {
+		fval, ok := strconv.ParseFloat(val, 64)
+
+		if ok != nil {
+			return errIncrParseErrRes, false
+		}
+
+		fval++
+		finalVal = strconv.FormatFloat(fval, 'f', 6, 64)
+	} else {
+		ival, ok := strconv.Atoi(val)
+
+		if ok != nil {
+			return errIncrParseErrRes, false
+		}
+
+		ival++
+		finalVal = strconv.Itoa(ival)
+	}
+
+	db.Set(key, finalVal)
+
+	return Response{StatusOK, "", map[string]string{"value": finalVal}}, true
 }
