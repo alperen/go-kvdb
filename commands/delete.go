840cce03ebc3fbf0e473837e318e11c3892acd6c
Alperen Turkoz <alperentrkz@gmail.com>
2020-02-29 14:06:57 +0300

    implement delete

---
+++ commands/delete.go
@@ -0,0 +1,19 @@
+package commands
+
+import "go-kvdb/database.go"
+
+var (
+	errDelKeyNotDefRes = Response{StatusErr, "Key field is not defined in received arguments", nil}
+)
+
+var Delete CommandFunc = func(db *database.Database, m map[string]string) (Response, bool) {
+	key, keyExists := m["key"]
+
+	if !keyExists {
+		return errDelKeyNotDefRes, false
+	}
+
+	ok := db.Delete(key)
+
+	return Response{StatusOK, "", nil}, ok
+}
